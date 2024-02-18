0234fbf19a624893b22bce590de661d3d4683d2e
igs-arakawa <s.arakawa@i-globalsociety.com>
2024-02-18 20:08:44 +0900

    init&fmt

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 terraform {
-    required_version = ">=0.13"
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 3.0"
-        }
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
+  }
 }
 
 provider "aws" {
   profile = "udemy-terraform-terraform"
-  region = "ap-northeast-1"
+  region  = "ap-northeast-1"
 }
 
 variable "project" {
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,2 @@
-project = "tastylog"
+project     = "tastylog"
 environment = "dev"
