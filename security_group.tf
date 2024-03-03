047f2d73e6a6e9b093bb756424b89392f98dfc23
arakaworld <arakaworld0619@gmail.com>
2024-03-03 21:07:29 +0900

    security group

---
+++ security_group.tf
@@ -0,0 +1,135 @@
+# -------------------------------------
+# Security Group
+# -------------------------------------
+# Web Server Security Group（HTTP/HTTPSが入ってくる、TCP3000が出ていく）
+resource "aws_security_group" "web_sg" {
+  name        = "${var.project}-${var.environment}-web-sg"
+  description = "web front role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-web-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+# インバウンドルール(HTTP)
+resource "aws_security_group_rule" "web_in_http" {
+  security_group_id = aws_security_group.web_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# インバウンドルール(HTTPS)
+resource "aws_security_group_rule" "web_in_https" {
+  security_group_id = aws_security_group.web_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# アウトバウンドルール(TCP3000)
+resource "aws_security_group_rule" "web_out_tcp3000" {
+  security_group_id        = aws_security_group.web_sg.id
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 3000
+  to_port                  = 3000
+  source_security_group_id = aws_security_group.app_sg.id
+}
+
+# Application Security Group（TCP3000が入ってくる、TCP3306が出ていく、HTTP/HTTPSがS3へ出ていく）
+# TODO: いったんセキュリティグループの箱だけ作る
+resource "aws_security_group" "app_sg" {
+  name        = "${var.project}-${var.environment}-app-sg"
+  description = "application server role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+# 運用管理用 Security Group（TCP3000が入ってくる、SSHが入ってくる、HTTP/HTTPSが出ていく）
+resource "aws_security_group" "opmng_sg" {
+  name        = "${var.project}-${var.environment}-opmng-sg"
+  description = "operation and management role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-opmng-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+# インバウンドルール(SSH)
+resource "aws_security_group_rule" "opmng_in_ssh" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# インバウンドルール(TCP3000)
+resource "aws_security_group_rule" "opmng_in_tcp3000" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 3000
+  to_port           = 3000
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# アウトバウンドルール(HTTP)
+resource "aws_security_group_rule" "opmng_out_http" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# アウトバウンドルール(HTTPS)
+resource "aws_security_group_rule" "opmng_out_https" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# RDS Security Group（TCP3306が入ってくる、TCP3000が出ていく）
+resource "aws_security_group" "db_sg" {
+  name        = "${var.project}-${var.environment}-db-sg"
+  description = "database role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-db-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+# インバウンドルール(SSH)
+resource "aws_security_group_rule" "db_in_tcp3306" {
+  security_group_id        = aws_security_group.db_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 3306
+  to_port                  = 3306
+  source_security_group_id = aws_security_group.app_sg.id
+}
