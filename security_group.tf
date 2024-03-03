b92b14356aaa63bb998ee558fd058aed5587d419
arakaworld <arakaworld0619@gmail.com>
2024-03-03 21:21:59 +0900

    fix security group

--- security_group.tf
+++ security_group.tf
@@ -58,6 +58,46 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+# インバウンドルール(TCP3000)
+resource "aws_security_group_rule" "app_in_tcp3000" {
+  security_group_id        = aws_security_group.app_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 3000
+  to_port                  = 3000
+  source_security_group_id = aws_security_group.web_sg.id
+}
+
+# アウトバウンドルール(s3/http)
+resource "aws_security_group_rule" "app_out_http" {
+  security_group_id = aws_security_group.app_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]
+}
+
+# アウトバウンドルール(s3/https)
+resource "aws_security_group_rule" "app_out_https" {
+  security_group_id = aws_security_group.app_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]
+}
+
+# アウトバウンドルール(TCP3306)
+resource "aws_security_group_rule" "app_out_tcp3306" {
+  security_group_id        = aws_security_group.app_sg.id
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 3306
+  to_port                  = 3306
+  source_security_group_id = aws_security_group.db_sg.id
+}
+
 # 運用管理用 Security Group（TCP3000が入ってくる、SSHが入ってくる、HTTP/HTTPSが出ていく）
 resource "aws_security_group" "opmng_sg" {
   name        = "${var.project}-${var.environment}-opmng-sg"
